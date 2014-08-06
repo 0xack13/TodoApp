2a90cc63c498123d2becb29c30cb05077f701980
Saleh Albatati <0xack13@gmail.com>
2014-08-06 20:28:52 +0300

    Initial Commit

---
+++ TodoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TodoApp
+//
+//  Created by 0xack13 on 8/6/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TodoApp
+//
+//  Created by 0xack13 on 8/6/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
