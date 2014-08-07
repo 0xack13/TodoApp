1d5b4eb402dfc3fd368b1de7daaefeaa987d7034
Saleh Albatati <0xack13@gmail.com>
2014-08-07 04:31:30 +0300

    Edit Task ViewController

---
+++ TATaskEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  TATaskEditViewController.h
+//  TodoApp
+//
+//  Created by 0xack13 on 8/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TATaskEditViewController : UITableViewController
+
+@end
---
+++ TATaskEditViewController.m
@@ -0,0 +1,45 @@
+//
+//  TATaskEditViewController.m
+//  TodoApp
+//
+//  Created by 0xack13 on 8/7/14.
+//  Copyright (c) 2014 0xack13. All rights reserved.
+//
+
+#import "TATaskEditViewController.h"
+
+@interface TATaskEditViewController ()
+
+@end
+
+@implementation TATaskEditViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
